fbc9e2044d13fdc529451e0122980ddcdad7a10c
harry2491 <hharwinderkaur@unomaha.edu>
2018-10-20 19:58:38 -0500

    Added my part

--- msa_app/views.py
+++ msa_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from .forms import *
 
@@ -13,3 +13,25 @@ def matches(request):
     matches = Match.objects.filter(created_date__lte=timezone.now())
     return render(request, 'custom/matches.html', {'match': matches})
 # Create your views here.
+
+
+def team_edit(request,pk):
+    team = get_object_or_404(Team,pk=pk)
+    if request.method == "POST":
+        #update
+        form = Team(request.POST,instance=team)
+
+    if form.is_valid():
+        team = form.save(commit=False)
+        team.update_date = timezone.now()
+        team.save()
+        team = Team.objects.filter(create_date_lte=timezone.now())
+        return render(request, 'custom/team_edit.html', {'team': team})
+
+    else:
+        # edit
+        form = Team(instance = team)
+    return render(request, 'msa/team_edit.html', {'form': form})
+
+
+
--- msa_default/urls.py
+++ msa_default/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.contrib.auth.views import LoginView, LogoutView
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('msa_app.urls')),
     re_path(r'^accounts/login/$', LoginView.as_view(template_name='registration/login.html'), name="login"),
     re_path(r'^accounts/logout/$', LogoutView.as_view(), LogoutView.next_page, name="logout"),
+
 ]
