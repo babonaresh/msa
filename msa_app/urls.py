819996e2a1cd4126363db7fe81e9649810b5fb52
harry2491 <hharwinderkaur@unomaha.edu>
2018-10-21 13:52:38 -0500

    Added Edit Team and New Team functionality

--- msa_app/urls.py
+++ msa_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     re_path(r'^home/$', views.home, name='home'),
     path('team_list', views.team_list, name='team_list'),
     path('matches', views.matches, name='matches'),
+    path('team/<int:pk>/edit/', views.team_edit, name='team_edit'),
+    path('team/new/', views.team_new, name='team_new'),
 ]
