f68d2a428648d512540c3bd2ab7ff4b8220019b6
babonaresh <nbashaboena@unomaha.edu>
2018-11-11 18:28:21 -0600

    added player add/edit/delete

--- msa_app/forms.py
+++ msa_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Team
+from .models import Team,Player
 
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ('team_name', 'team_logo', 'school','active_status','coach')
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = ('first_name', 'last_name','email','phone','team','eligibility_status','team_role', 'squad_position', 'street', 'city','state','zipcode')
